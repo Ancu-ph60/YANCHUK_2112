78b72c6e584110cf69753f72e543b1d92ffe7f3c
b2112 <viacheslav.martson@gmail.com>
2024-12-22 17:53:08 +0200

    lesson7-8

--- Lesson 7-8.py
+++ Lesson 7-8.py
@@ -6,9 +6,38 @@ sum_negetive = 0
 
 
 for num in lst:
-    if num < 0: sum += num
-print("Sum negative item list:", sum_negative)
+    if num < 0: sum_negetive += num
+print("Sum negative item list:", sum_negetive)
 
 
 
-sum_of_even = 0
+sum_of_odd = 0
+for num in lst:
+    if num % 2 != 0:
+        sum_of_odd += num
+print("Sum negative item list:",sum_of_even )
+
+
+sum_of_odd = 0
+for num in lst:
+    if num % 2 != 0:
+        sum_of_odd += num
+    print("Sum negative item list:", sum_of_even)
+
+    sum_elements_mult_3=0
+    for i in range(len(lst)):
+        if 1 % 3 == 0 :
+            sum_elements_mult_3 += lst[i]
+        print("Sum negative item list:", sum_elements_mult_3)
+
+mult_runge = 1
+min_value_list = min(lst)
+max_value_list = max(lst)
+index_min = lst.index(min_value_list)
+index_max = lst.index(max_value_list)
+if index_max<index_min:
+    index_max,index_min=index_min,index_max
+for i in range(index_min,index_max,1):
+    mult_runge *= lst[i]
+print("product of minimum and maximum numbers:")
+
